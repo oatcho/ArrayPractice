428b65e8c989c941c100844e24b0240aabf5d80f
Omar Atcho <omar@Omars-MacBook-Pro.local>
2019-01-23 11:12:28 -0500

    User Input working, Print of Array Working, Beginning to work on averaging temperatures

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner key = new Scanner(System.in);
+
+        double[] dailyAverageTemp = new double[7];
+
+        System.out.println("Please enter the temperatures in Fahrenheit for the next 7 days: ");
+
+        dailyAverageTemp[0] = key.nextDouble();
+        dailyAverageTemp[1] = key.nextDouble();
+        dailyAverageTemp[2] = key.nextDouble();
+        dailyAverageTemp[3] = key.nextDouble();
+        dailyAverageTemp[4] = key.nextDouble();
+        dailyAverageTemp[5] = key.nextDouble();
+        dailyAverageTemp[6] = key.nextDouble();
+
+        for (int i=0; i<dailyAverageTemp.length; i++) {
+            System.out.println (dailyAverageTemp [i]);}
+
+
     }
 }
