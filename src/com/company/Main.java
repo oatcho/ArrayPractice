647fd0ece2f61f13453224d72e115b0aefa5baea
Omar Atcho <omar@Omars-MacBook-Pro.local>
2019-01-23 16:23:15 -0500

    Accepts user input, converts temp fine, trying to create new array

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,9 +9,12 @@ public class Main {
         Scanner key = new Scanner(System.in);
 
         double[] dailyAverageTemp = new double[7];
+        double[] dailyAverageTempInCel = new double [7];
         double n = dailyAverageTemp.length;
         double sumF = 0;
         double averageF;
+        double sumC = 0;
+        double averageC;
 
         System.out.println("Please enter the temperatures in Fahrenheit for the next 7 days: ");
 
@@ -23,14 +26,37 @@ public class Main {
         dailyAverageTemp[5] = key.nextDouble();
         dailyAverageTemp[6] = key.nextDouble();
 
-        for (int i=0; i<dailyAverageTemp.length; i++) {
-            System.out.println (dailyAverageTemp [i]);
-            sumF = sumF + dailyAverageTemp[i];
-            }
+//        for (int i=0; i<dailyAverageTemp.length; i++) {
+////            System.out.println (dailyAverageTemp [i]);
+//            sumF = sumF + dailyAverageTemp[i];
+//            }
 
-        averageF =  sumF / n;
+        for (double tempInF: dailyAverageTemp) {
+            sumF = sumF + tempInF;
+//            System.out.println(tempInF);
+        }
 
-        System.out.println ("Average temperature in Fahrenheit for the week is: " + averageF);
+//        for (double i = 0; i<dailyAverageTemp.length; i++) {
+//
+//            i=(((i-32)/1.8));
+//            System.out.println(i);
+//            break;
+//        }
+
+        for (double tempInC: dailyAverageTemp) {
+
+            tempInC=((tempInC-32)/1.8);
+            System.out.println(dailyAverageTempInCel);
+//            System.arraycopy(dailyAverageTemp, 0, dailyAverageTempInCel, 0, 7);
+//            System.out.println(dailyAverageTempInCel);
+        }
+
+        averageF = sumF / n;
+        averageC = sumC / n;
+
+//        System.out.println ("The average temperature in Fahrenheit for the week is: " + averageF);
+
+//        System.out.println ("The average temperature in Celcius for the week is: " + averageC);
 
 
 
